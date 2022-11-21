6efc97840ef9099f43bd218f0aaf04d223f949dd
Yue Duan <102155262+NJUyued@users.noreply.github.com>
2022-11-21 19:07:51 +0800

    22-11-21

--- train_mutex.py
+++ train_mutex.py
@@ -337,7 +337,7 @@ if __name__ == "__main__":
     Data Configurations
     '''
     
-    parser.add_argument('--data_dir', type=str, default='../MutexMatch_/data/')
+    parser.add_argument('--data_dir', type=str, default='data')
     parser.add_argument('--dataset', type=str, default='cifar10')
     parser.add_argument('--train_sampler', type=str, default='RandomSampler')
     parser.add_argument('--num_classes', type=int, default=10)
